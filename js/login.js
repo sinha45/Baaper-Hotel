f13ff1c7ed0f1eec2e4a510b4092334ac6c3f51b
sinha45 <alfa45leonidas@gmail.com>
2022-09-26 01:09:02 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,26 @@
+//**step1: add click event handler with submit button */
+
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // ***step2: get the email address inside the email input field***/
+    //*** always remember to use .value to get text from an input field */
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+
+    //**step3: get password */
+    //**3.a: set id on the html element */
+    //**3.b: get the element */
+    //**3.c: get the value form the element */
+
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    //**Do not verify email password on the client side */
+    //**step-4: verify email and password & check whether valid user or not */
+
+    if (email === 'sontan@baap.com' && password === 'secret') {
+        console.log('valid user');
+    }
+    else {
+        console.log('invalid user');
+    }
+});
